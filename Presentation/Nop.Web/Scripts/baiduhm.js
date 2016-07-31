8802725fd6600ad03993fd28dce29a54bb00d400
alvin.liu <somtomking@live.cn>
2016-07-31 15:52:32 +0700

    添加百度统计的功能

---
+++ Presentation/Nop.Web/Scripts/baiduhm.js
@@ -0,0 +1,9 @@
+﻿
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?8f947ad1405d543538f1269aa00c99ff";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
