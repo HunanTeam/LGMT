c6d6841ded258dac2dda836e2f639b38cf7dbf67
alvin.liu <somtomking@live.cn>
2016-08-14 10:36:03 +0700

    添加sql，添加一些配置

---
+++ Presentation/Nop.Web/App_Data/更新脚本/03.添加一些配置.sql
@@ -0,0 +1,7 @@
+﻿--添加移动端主题
+INSERT INTO [dbo].[Setting]
+           ([Name]
+           ,[Value]
+           ,[StoreId])
+     VALUES
+         ('storeinformationsettings.defaultmoblietheme','Mobile',0)
