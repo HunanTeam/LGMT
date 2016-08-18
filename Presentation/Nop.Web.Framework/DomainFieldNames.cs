9bcb3060391b9aa2e1f0bd314f611b6b0896361c
alvin.liu <somtomking@live.cn>
2016-08-18 20:40:42 +0700

    修改前台文字的显示

---
+++ Presentation/Nop.Web.Framework/DomainFieldNames.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Web.Framework
+{
+    public class DomainFieldNames
+    {
+        
+
+        /// <summary>
+        /// 窗纱
+        /// </summary>
+        public const string Product_Detail_ShaChuang= "窗纱";
+        /// <summary>
+        /// 遮光布
+        /// </summary>
+        public const string Product_Detail_ZheGuangBu = "遮光布";
+        /// <summary>
+        /// 帘头
+        /// </summary>
+        public const string Product_Detail_LianTou = "帘头";
+        /// <summary>
+        /// 平板式
+        /// </summary>
+        public const string Product_Detail_Name_4 = "";
+        /// <summary>
+        /// 打褶式
+        /// </summary>
+        public const string Product_Detail_Name_5 = "";
+        /// <summary>
+        /// 水波帘
+        /// </summary>
+        public const string Product_Detail_Name_6 = "";
+        /// <summary>
+        /// 安装房间
+        /// </summary>
+        public const string Product_Detail_AnZhuangFangJian = "安装房间";
+        /// <summary>
+        /// 安装位置
+        /// </summary>
+        public const string Product_Detail_AnZHuangWeiZhi = "安装位置";
+        /// <summary>
+        /// 尺寸
+        /// </summary>
+        public const string Product_Detail_ChiCun= "尺寸";
+        /// <summary>
+        /// 长度
+        /// </summary>
+        public const string Product_Detail_ChangDu= "长度";
+        /// <summary>
+        /// 高度
+        /// </summary>
+        public const string Product_Detail_GaoDu= "高度";
+        /// <summary>
+        /// 轨道
+        /// </summary>
+        public const string Product_Detail_GuiDao = "轨道";
+        /// <summary>
+        /// 颜色
+        /// </summary>
+        public const string Product_Detail_YanSe = "颜色";
+    }
+}
